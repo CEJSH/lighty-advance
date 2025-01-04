65e9ae9e1cbce53d64b58328ab508f39621bfc3b
CEJSH <eun2763@naver.com>
2025-01-04 17:55:32 +0900

    lint error 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import App from "./App.tsx";
 import { AlertContextProvider } from "./contexts/AlertContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
