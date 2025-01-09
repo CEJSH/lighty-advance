14f9a7678e5ca03ac63a4e9b1d129a9b6f1fa2b7
CEJSH <eun2763@naver.com>
2025-01-09 23:10:04 +0900

    recoil setting

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { AlertContextProvider } from "./contexts/AlertContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <AlertContextProvider>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </AlertContextProvider>
   </QueryClientProvider>
 );
